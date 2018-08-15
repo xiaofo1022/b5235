6e90062664b7c18817859b29bf622ce8b8e04548
yuchao <553216263@qq.com>
2018-08-15 13:13:03 +0800

    Add MQ to project

--- src/main/java/com/xiaofo1022/b5235/mq/producer/WxMsgProducer.java
+++ src/main/java/com/xiaofo1022/b5235/mq/producer/WxMsgProducer.java
@@ -5,6 +5,7 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.MessageProperties;
 import com.xiaofo1022.b5235.mq.config.MQConfig;
 import com.xiaofo1022.b5235.mq.core.MQCore;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,23 +13,21 @@ import java.util.concurrent.TimeUnit;
  * @author yuchao
  * @since 2018-08-15 上午11:07
  */
+@Slf4j
 public class WxMsgProducer {
 
   public void initMQBaseConfig() throws Exception {
-    try (
-        Connection connection = MQCore.getConnection();
-        Channel channel = connection.createChannel()
-    ) {
+    Connection connection = MQCore.getConnection();
+    try (Channel channel = connection.createChannel()) {
       channel.exchangeDeclare(MQConfig.WX_EXCHANGE_NAME, "direct", true, false, null);
+      channel.queueDeclare(MQConfig.WX_QUEUE_NAME, true, false, false, null);
       channel.queueBind(MQConfig.WX_QUEUE_NAME, MQConfig.WX_EXCHANGE_NAME, MQConfig.WX_ROUTING_KEY);
     }
   }
 
   public boolean sendSimpleMsg(String msgBody) throws Exception {
-    try (
-        Connection connection = MQCore.getConnection();
-        Channel channel = connection.createChannel()
-    ) {
+    Connection connection = MQCore.getConnection();
+    try (Channel channel = connection.createChannel()) {
       channel.basicPublish(MQConfig.WX_EXCHANGE_NAME, MQConfig.WX_ROUTING_KEY, MessageProperties.PERSISTENT_TEXT_PLAIN, msgBody.getBytes());
     }
     return true;
@@ -39,7 +38,9 @@ public class WxMsgProducer {
     msgProducer.initMQBaseConfig();
     int msgCount = 0;
     while (true) {
-      msgProducer.sendSimpleMsg("Msg from b5235 [" + (msgCount++) + "]");
+      String msgBody = "Msg from b5235 [" + (msgCount++) + "]";
+      log.info("Send msg to mq: " + msgBody);
+      msgProducer.sendSimpleMsg(msgBody);
       TimeUnit.SECONDS.sleep(1);
     }
   }
